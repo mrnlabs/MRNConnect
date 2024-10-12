bc00c1e780c5fe6b23e9b00645e73713e78ca104
Mazisi Msebele <56942052+mazisi@users.noreply.github.com>
2024-10-12 20:21:00 +0200

    Update MRNConnect.jsx

--- src/MRNConnect.jsx
+++ src/MRNConnect.jsx
@@ -3,29 +3,29 @@ import { JitsiMeeting } from "@jitsi/react-sdk";
 export default function MRNConnect() {
     return (
     <div>
-<JitsiMeeting
-    domain = { import.meta.env.VITE_JITSI_DOMAIN }
-    roomName = "MRN Connect"
-    configOverwrite = {{
-        startWithAudioMuted: true,
-        disableModeratorIndicator: true,
-        startScreenSharing: true,
-        enableEmailInStats: false
-    }}
-    interfaceConfigOverwrite = {{
-        DISABLE_JOIN_LEAVE_NOTIFICATIONS: true
-    }}
-    userInfo = {{
-        displayName: 'MRN User'
-    }}
-    onApiReady = { (externalApi) => {
-        // we will go to laravel
-    } }
-    getIFrameRef = { (iframeRef) => { 
-        iframeRef.style.height = '700px';
-        iframeRef.style.width = '80vw';
-         } }
-/>
+        <JitsiMeeting
+            domain = { import.meta.env.VITE_JITSI_DOMAIN }//we will have to host it ourselves at some point
+            roomName = "MRN Connect"
+            configOverwrite = {{
+                startWithAudioMuted: true,
+                disableModeratorIndicator: true,
+                startScreenSharing: true,
+                enableEmailInStats: false
+            }}
+            interfaceConfigOverwrite = {{
+                DISABLE_JOIN_LEAVE_NOTIFICATIONS: true
+            }}
+            userInfo = {{
+                displayName: 'MRN User'
+            }}
+            onApiReady = { (laravelApi) => {
+                // we will go to laravel
+            } }
+            getIFrameRef = { (iframeRef) => { 
+                iframeRef.style.height = '700px';
+                iframeRef.style.width = '80vw';
+                } }
+        />
     </div>
     )
 }
